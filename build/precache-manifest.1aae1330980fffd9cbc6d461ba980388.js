7b3cbbcf28c3a4ac207dbfd6598276fce125f802
igorashs <igorashsirbu@gmail.com>
2020-09-10 18:48:11 +0300

    update build

---
+++ build/precache-manifest.1aae1330980fffd9cbc6d461ba980388.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703d59a7acc2d014a8a529481abee2cf",
+    "url": "/react-blog-editor/index.html"
+  },
+  {
+    "revision": "ccd72287efc5f88c1f06",
+    "url": "/react-blog-editor/static/css/3.b5819c7b.chunk.css"
+  },
+  {
+    "revision": "692a81bae8b7de74417b",
+    "url": "/react-blog-editor/static/css/4.0fcc1597.chunk.css"
+  },
+  {
+    "revision": "ea629794cb6acd566105",
+    "url": "/react-blog-editor/static/css/main.48212a66.chunk.css"
+  },
+  {
+    "revision": "e3d60d7a350d6c2c2f51",
+    "url": "/react-blog-editor/static/js/2.d4626d72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-blog-editor/static/js/2.d4626d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd72287efc5f88c1f06",
+    "url": "/react-blog-editor/static/js/3.29221b43.chunk.js"
+  },
+  {
+    "revision": "692a81bae8b7de74417b",
+    "url": "/react-blog-editor/static/js/4.cc17995b.chunk.js"
+  },
+  {
+    "revision": "ea629794cb6acd566105",
+    "url": "/react-blog-editor/static/js/main.c024712a.chunk.js"
+  },
+  {
+    "revision": "b45ada5260115f09fce2",
+    "url": "/react-blog-editor/static/js/runtime-main.0d1d9a46.js"
+  }
+]);
